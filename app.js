3a055a5c40273a3400db9beaae05946d67ff492e
Michelle Clement <michellelclement@gmail.com>
2022-02-16 10:05:13 +0000

    Add classes to newly created card elements

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ const cardGenerator = () => {
     const card = document.createElement("div");
     const face = document.createElement("img");
     const back = document.createElement("div");
+    //Add classes to new elements
+    card.classList = 'card';
+    face.classList = 'face';
+    back.classlist = 'back';
 };
 
 cardGenerator();
