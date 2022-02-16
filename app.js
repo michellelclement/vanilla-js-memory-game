d1c457c2ebe9ec7b64953100839ef06086c9068a
Michelle Clement <michellelclement@gmail.com>
2022-02-16 09:23:11 +0000

    Add image sources for data

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const playerLives = 5;
 
 //Link text 
 playerLivesCount.textContent = playerLives;
+
+//Generate the data to use for the cards
+const cardGenerator = () => [
+    { imgSrc: "images/beatles.jpeg", name: "beatles" }
+    { imgSrc: "images/blink182.jpeg", name: "blink 182" }
+    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" }
+    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" }
+    { imgSrc: "images/joy-division.jpeg", name: "joy division" }
+    { imgSrc: "images/ledzep.jpeg", name: "ledzep" }
+    { imgSrc: "images/metallica.jpeg", name: "metallica" }
+    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" }
+    { imgSrc: "images/beatles.jpeg", name: "beatles" }
+    { imgSrc: "images/blink182.jpeg", name: "blink 182" }
+    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" }
+    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" }
+    { imgSrc: "images/joy-division.jpeg", name: "joy division" }
+    { imgSrc: "images/ledzep.jpeg", name: "ledzep" }
+    { imgSrc: "images/metallica.jpeg", name: "metallica" }
+    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" }
+]
