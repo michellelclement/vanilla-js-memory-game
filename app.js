264185b0b85e8e9f967c14b6e3f4239ccd480217
Michelle Clement <michellelclement@gmail.com>
2022-02-16 09:16:19 +0000

    Create first variables and add player lives text

---
+++ app.js
@@ -0,0 +1,8 @@
+//Grab a couple of things
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+const playerLives = 5;
+
+
+//Link text 
+playerLivesCount.textContent = playerLives;
