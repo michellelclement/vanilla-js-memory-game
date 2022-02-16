e370a6201dfd40ebf6682a924577f14c75b43176
Michelle Clement <michellelclement@gmail.com>
2022-02-16 09:26:52 +0000

    This shows the items in the array on the console, but not randomised yet

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ const playerLives = 5;
 playerLivesCount.textContent = playerLives;
 
 //Generate the data to use for the cards
-const cardGenerator = () => [
-    { imgSrc: "images/beatles.jpeg", name: "beatles" }
-    { imgSrc: "images/blink182.jpeg", name: "blink 182" }
-    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" }
-    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" }
-    { imgSrc: "images/joy-division.jpeg", name: "joy division" }
-    { imgSrc: "images/ledzep.jpeg", name: "ledzep" }
-    { imgSrc: "images/metallica.jpeg", name: "metallica" }
-    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" }
-    { imgSrc: "images/beatles.jpeg", name: "beatles" }
-    { imgSrc: "images/blink182.jpeg", name: "blink 182" }
-    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" }
-    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" }
-    { imgSrc: "images/joy-division.jpeg", name: "joy division" }
-    { imgSrc: "images/ledzep.jpeg", name: "ledzep" }
-    { imgSrc: "images/metallica.jpeg", name: "metallica" }
-    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" }
-]
+const getData = () => [
+    { imgSrc: "images/beatles.jpeg", name: "beatles" },
+    { imgSrc: "images/blink182.jpeg", name: "blink 182" },
+    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" },
+    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" },
+    { imgSrc: "images/joy-division.jpeg", name: "joy division" },
+    { imgSrc: "images/ledzep.jpeg", name: "ledzep" },
+    { imgSrc: "images/metallica.jpeg", name: "metallica" },
+    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" },
+    { imgSrc: "images/beatles.jpeg", name: "beatles" },
+    { imgSrc: "images/blink182.jpeg", name: "blink 182" },
+    { imgSrc: "images/fkatwigs.jpeg", name: "fka twigs" },
+    { imgSrc: "images/fleetwood.jpeg", name: "fleetwood" },
+    { imgSrc: "images/joy-division.jpeg", name: "joy division" },
+    { imgSrc: "images/ledzep.jpeg", name: "ledzep" },
+    { imgSrc: "images/metallica.jpeg", name: "metallica" },
+    { imgSrc: "images/pinkfloyd.jpeg", name: "pinkfloyd" },
+];
+
+//Randomise funcion
+const randomize = () => {
+    const cardData = getData();
+    console.log(cardData);
+};
+
+randomize();
