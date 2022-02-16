301dd09e6769b09c04bb629607356ae0a7bd67cc
Michelle Clement <michellelclement@gmail.com>
2022-02-16 14:07:06 +0000

    If matched, remove ability to click them again and they do not dissappear

--- app.js
+++ app.js
@@ -75,6 +75,10 @@ const checkCards = (e) => {
             flippedCards[1].getAttribute('name')
             ) {
         console.log('match');
+        flippedCards.forEach(card => {
+            card.classList.remove('flipped');
+            card.style.pointerEvents = 'none';
+        })
         } else {
             console.log('wrong');
             flippedCards.forEach(card => {
